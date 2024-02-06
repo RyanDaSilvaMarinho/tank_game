aa6cf5f58150fec7f6d722d749a29f5cc34cc176
RyanDaSilvaMarinho <rdsm.snf23@uea.edu.br>
2024-02-06 17:18:00 -0400

    adcionando algumas funcionalidades

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from bullet import bullet
 from tank import tank
 
-a = tank('a')
-b = tank('b')
+a = tank('tank a')
+b = tank('tank b')
 
 bullet1 = bullet('bullet',a)
 bullet1.scope()
 bullet1.shoot(b)
 bullet1.destroy()
 
-a.movement('shoot tank b')
+a.movement('tank a turn left')
+b.tank_status('tank b is reaching tank a')
+a.tank_status('shoot tank b')
 b.movement('tank b exploded')
 a.print_movement()
+b.print_tank_status()
+a.print_tank_status()
+b.print_movement()
+
--- tank.py
+++ tank.py
@@ -4,10 +4,13 @@ class tank:
     def __init__(self, name):
         self.name = name
         self.movements = []
-
+        self.tank_action = []
     def movement(self, move):
         self.movements.append(move)
-
+    def tank_status(self, move):
+        self.tank_action.append(move)
+    def print_tank_status(self):
+        print(self.tank_action)
     def print_movement(self):
         print(self.movements)
 
